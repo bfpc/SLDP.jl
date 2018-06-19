e4a953bc01ac2dddd9a2fe2658189fecf1e085bb
Bernardo Freitas Paulo da Costa <bernardo.da-costa@m4x.org>
2018-06-19 08:37:55 -0300

    Move add_aldcut!: solver.jl -> valuefunction.jl

--- src/solver.jl
+++ src/solver.jl
@@ -27,28 +27,6 @@ function set_xhat_av!(sp::JuMP.Model, xhat)
   end
 end
 
-function add_aldcut!(m::SDDP.SDDPModel, sp::JuMP.Model, v, l, rho)
-  _theta = SDDP.valueoracle(sp).theta
-  t = SDDP.ext(sp).stage
-  xhat = SDDP.getstage(m, t).state
-  niter = length(m.log)
-
-  push!(aldcuts(sp), ALDCut(copy(xhat), v, l, rho))
-
-  aff_expr = v
-  for (st,xi,li) in zip(aldstates(sp), xhat, l)
-    pos_part = @variable(sp, lowerbound=0, basename="theta_pos_"*string(niter))
-    neg_part = @variable(sp, lowerbound=0, basename="theta_neg_"*string(niter))
-    bin_choose = @variable(sp, category=:Bin, basename="theta_bin_"*string(niter))
-
-    @constraint(sp, pos_part <= bin_choose * (st.ub - xi))
-    @constraint(sp, neg_part <= (1-bin_choose) * (xi - st.lb))
-    @constraint(sp, pos_part - neg_part == st.xout - xi)
-    aff_expr += li*(st.xout - xi) - rho*sum(pos_part+neg_part)
-  end
-  @constraint(sp, _theta >= aff_expr)
-end
-
 function saveInfeasible(sp :: JuMP.Model)
   filepath = joinpath(pwd(), "infeasible_subproblem.lp")
   JuMP.writeLP(sp, filepath, genericnames=false)
--- src/valuefunction.jl
+++ src/valuefunction.jl
@@ -1,3 +1,25 @@
+function add_aldcut!(m::SDDP.SDDPModel, sp::JuMP.Model, v, l, rho)
+  _theta = SDDP.valueoracle(sp).theta
+  t = SDDP.ext(sp).stage
+  xhat = SDDP.getstage(m, t).state
+  niter = length(m.log)
+
+  push!(aldcuts(sp), ALDCut(copy(xhat), v, l, rho))
+
+  aff_expr = v
+  for (st,xi,li) in zip(aldstates(sp), xhat, l)
+    pos_part = @variable(sp, lowerbound=0, basename="theta_pos_"*string(niter))
+    neg_part = @variable(sp, lowerbound=0, basename="theta_neg_"*string(niter))
+    bin_choose = @variable(sp, category=:Bin, basename="theta_bin_"*string(niter))
+
+    @constraint(sp, pos_part <= bin_choose * (st.ub - xi))
+    @constraint(sp, neg_part <= (1-bin_choose) * (xi - st.lb))
+    @constraint(sp, pos_part - neg_part == st.xout - xi)
+    aff_expr += li*(st.xout - xi) - rho*sum(pos_part+neg_part)
+  end
+  @constraint(sp, _theta >= aff_expr)
+end
+
 function modify_ald_valuefunction!(m::SDDP.SDDPModel{V}, settings::SDDP.Settings, sp::JuMP.Model) where V<:SDDP.DefaultValueFunction
     ex = SDDP.ext(sp)
     vf = SDDP.valueoracle(sp)
