37175be01b857ba1d4e4fa847e4f530f6ab4817b
Bernardo Freitas Paulo da Costa <bernardo.da-costa@m4x.org>
2018-08-17 07:36:14 -0300

    Replace qbar by Qtilde and Qfrak
    These two functions now estimate the future cost at a given stage in the
    two SDDP-like ways:
    - Qtilde runs through all realizations of the random variable, solves
      the next stage problem, and averages (missing Markov averaging)
    - Qfrak uses cuts for the current stage
    
    By estimating the difference between Qtilde and Qfrak, we can get a good
    idea of the quality of the estimate.

--- src/utils.jl
+++ src/utils.jl
@@ -1,33 +1,85 @@
 import JuMP
 import SDDP
 
-function estimatevf(sp::JuMP.Model, ts)
-  vf = []
-  ac = aldcuts(sp)
-  if length(ac) == 0
-    return zeros(ts)
-  end
-  for c in ac
-    vf_i = [c.v + dot(c.l, ti - c.xi) - c.rho*norm(ti - c.xi, 1) for ti in ts]
-    push!(vf, vf_i)
-  end
-  maximum(hcat(vf...), 2)[:,1]
-end
-
-function qbar(sp::JuMP.Model)
-  ex = SDDP.ext(sp)
-  if SDDP.hasnoises(sp)
-    vs = []
-    for i in 1:length(ex.noiseprobability)
-      SDDP.setnoise!(sp, ex.noises[i])
-      JuMP.solve(sp)
-      push!(vs, JuMP.getobjectivevalue(sp))
+function estimatevf(sp::JuMP.Model, states...)
+    vf = []
+    ac = ASDDiP.aldcuts(sp)
+    if length(ac) == 0
+        b = SDDP.ext(sp).problembound
+        if length(states) == 1
+            npts = length(states[1])
+        else
+            npts = length(states[1])*length(states[2])
+        end
+        return b * ones(npts)
     end
-    return mean(vs)
-  else
-    JuMP.solve(sp)
-    return JuMP.getobjectivevalue(sp)
-  end
+    for c in ac
+        if length(states) == 1
+           ts = states[1]
+        else
+            ts = SDDP.mesh(states...)
+        end
+        vf_i = [c.v + dot(c.l, ti - c.xi) - c.rho*norm(ti - c.xi, 1) for ti in ts]
+        push!(vf, vf_i)
+    end
+    maximum(hcat(vf...), 2)[:,1]
 end
 
+function Qfrak(m,t,ms, states::Union{Float64, AbstractVector{Float64}}...)
+    sp = SDDP.getsubproblem(m,t,ms)
+    vf = SDDP.valueoracle(sp)
+    _, y_cont = SDDP.processvaluefunctiondata(vf, true, states...)
+    y_ald = estimatevf(sp, states...)
+    maximum(hcat(y_cont, y_ald), 2)[:,1]
+end
 
+function setstate!(sp::JuMP.Model, vs::AbstractVector{Float64})
+    for (st, v) in zip(SDDP.states(sp), vs)
+        lb = JuMP.getlowerbound(st.variable)
+        ub = JuMP.getupperbound(st.variable)
+        if v < lb
+            SDDP.setvalue!(st, lb)
+        elseif v > ub
+            SDDP.setvalue!(st, ub)
+        else
+            SDDP.setvalue!(st, v)
+        end
+    end
+end
+
+function Qtilde(sp::JuMP.Model, state::AbstractVector{Float64})
+    setstate!(sp,state)
+    if SDDP.hasnoises(sp)
+        ex = SDDP.ext(sp)
+        vs = []
+        for i in 1:length(ex.noiseprobability)
+            SDDP.setnoise!(sp, ex.noises[i])
+            JuMP.solve(sp)
+            push!(vs, JuMP.getobjectivevalue(sp))
+        end
+        return mean(vs)
+    else
+        JuMP.solve(sp)
+        return JuMP.getobjectivevalue(sp)
+    end
+end
+
+function Qtilde(m,t,ms, states::Union{Float64, AbstractVector{Float64}}...; debug=false)
+    sp = SDDP.getsubproblem(m,t+1,ms)
+    ans = []
+    if length(states) == 2
+        m = SDDP.mesh(states...)
+    else
+        m = states[1]'
+    end
+    for i in 1:size(m,2)
+        if debug
+            print(i, ", ")
+        end
+        push!(ans, Qtilde(sp,m[:,i]))
+    end
+    if debug
+        println()
+    end
+    return ans
+end
