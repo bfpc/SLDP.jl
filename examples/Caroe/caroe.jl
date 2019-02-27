beb311dcadf14f3bb1d7ad55781f463537ce97eb
Bernardo Freitas Paulo da Costa <bernardo.da-costa@m4x.org>
2019-02-27 19:32:07 -0300

    Update caroe.jl to functional format

--- examples/Caroe/caroe.jl
+++ examples/Caroe/caroe.jl
@@ -7,35 +7,50 @@ import ASDDiP: prepareALD!, setASDDiPsolver!
 
 # =============
 # \rho policies
-Lip(t) = 30
-function rho_ramp(niter, t, i=1)
-  Lip(t) * clamp((niter-15)/15, 0, 1)
+function rho_ramp(niter, Lip, i=1)
+  Lip * clamp((niter-15)/15, 0.0, 1.0)
 end
 
-function rho_ramp_parallel(niter, t, i=1)
-  clamp((niter-15)/15, 0, Lip(t))
+function rho_ramp_parallel(niter, Lip, i=1)
+  clamp((niter-15)/15, 0.0, Lip)
 end
 
-function rho_ramp_parallel2(niter, t, i=1)
-  clamp((niter-15)/15, 0, 2*Lip(t))
+function rho_ramp_parallel2(niter, Lip, i=1)
+  clamp((niter-15)/15, 0.0, 2*Lip)
 end
 
-function rho_zero(niter, t, i=1)
-  0
+function rho_zero(niter, Lip, i=1)
+  0.0
 end
 
+# And dictionnary
+rho_fun = Dict(
+  :None => rho_zero,
+  :simple => rho_ramp,
+  :parallel => rho_ramp_parallel,
+  :parallel2 => rho_ramp_parallel2,
+  :opt_rho => rho_zero
+ )
 
-# ==========
-# Parameters
-if !isdefined(:ramp_mode)
-  ramp_mode = :None
+
+# ==============
+# Stage problems
+cost1 = [3/2 4]
+function stage1(sp)
+  x = sp[:x]
+  @stageobjective(sp, -sum(cost1*x))
 end
-if !isdefined(:niters)
-  niters = 0
+
+cost2 = [16 19 23 28]
+function stage2(sp, noise)
+  x0 = sp[:x0]
+  @variable(sp, y[1:4], Bin)
+  @rhsnoise(sp, w=noise, sum([2 3 4 5]*y) <= w[1] - x0[1])
+  @rhsnoise(sp, w=noise, sum([6 1 3 2]*y) <= w[2] - x0[2])
+  @stageobjective(sp, -sum(cost2*y))
 end
-if !isdefined(:num_noise)
-  num_noise = 2
-end
+
+function caroe_model(;nstages=2, ramp_mode=:None, num_noise=2)
 
 # =====
 # Noise
@@ -44,28 +59,12 @@ noisestep = 10/(num_noise - 1)
 noisegen = 1:num_noise
 noise = 5 + noisestep*[[i-1,j-1] for i = noisegen for j = noisegen]
 
-# ==============
-# Stage problems
-function stage1(sp)
-  x = sp[:x]
-  @stageobjective(sp, -sum([3/2 4]*x))
-end
-
-function stage2(sp)
-  x0 = sp[:x0]
-  @variable(sp, y[1:4], Bin)
-  @rhsnoise(sp, w=noise, sum([2 3 4 5]*y) <= w[1] - x0[1])
-  @rhsnoise(sp, w=noise, sum([6 1 3 2]*y) <= w[2] - x0[2])
-  @stageobjective(sp, -sum([16 19 23 28]*y))
-end
-
-
 # ==========
 # SDDP Model
 m = SDDPModel(
         sense             = :Min,
-        stages            = 2,
-        objective_bound   = -100,
+        stages            = nstages,
+        objective_bound   = -(sum(cost1)*5 + sum(cost2)*(nstages-1)),
         solver            = GurobiSolver(OutputFlag=0,TimeLimit=60)
        ) do sp, stage
 
@@ -75,7 +74,7 @@ m = SDDPModel(
     if stage == 1
       stage1(sp)
     else
-      stage2(sp)
+      stage2(sp, noise)
     end
 
     # =================================
@@ -83,26 +82,31 @@ m = SDDPModel(
     setASDDiPsolver!(sp)
 end
 
-# ===============
-# Prepare & solve
-if ramp_mode == :None
-  prepareALD!(m, rho_zero)
-elseif ramp_mode == :simple
-  prepareALD!(m, rho_ramp)
-elseif ramp_mode == :parallel
-  prepareALD!(m, rho_ramp_parallel)
-elseif ramp_mode == :parallel2
-  prepareALD!(m, rho_ramp_parallel2)
-elseif ramp_mode == :opt_rho
-  prepareALD!(m, rho_zero)
+# ==================
+# Lipschitz constant
+function Lip(t)
+    return 30
+end
+
+# =======
+# Prepare
+if ramp_mode == :opt_rho
+  prepareALD!(m, Lip, rho_zero)
   for stage in SDDP.stages(m)
     for sp in SDDP.subproblems(stage)
       JuMP.setsolvehook(sp, ASDDiP.ASDDiPsolve_optrho!)
     end
   end
 else
+  try
+    prepareALD!(m, Lip, rho_fun[ramp_mode])
+  catch
   warn("Invalid ramp mode $(ramp_mode).  Not using ALD cuts, only Strenghtened Benders.")
-  prepareALD!(m, rho_zero)
+    prepareALD!(m, Lip, rho_zero)
+  end
+end
+
+return m
 end
 
 sim = SDDP.MonteCarloSimulation(
@@ -111,7 +115,7 @@ sim = SDDP.MonteCarloSimulation(
      terminate = false
     )
 
-if niters > 0
+function caroe_solve(m, niters)
     @time solvestatus = SDDP.solve(m,
         iteration_limit = niters,
         simulation = sim
