d6d7982cd23097abf438eb9cdf6f6c21c51d993e
Bernardo Freitas Paulo da Costa <bernardo.da-costa@m4x.org>
2019-04-30 19:50:20 -0400

    Use smaller shifts, and export them

--- examples/control/constants.jl
+++ examples/control/constants.jl
@@ -6,7 +6,7 @@ A_noise   = 0.4
 num_noise = 5
 noise = randn(num_noise)
 noise = A_noise * [noise; -noise]
-shift = [0.0, 3.0, 2.0, 0.0, 0.0, -2.0, -3.0, 0.0]
+shift = [0.0, 1.0, 2.0, 0.0, 0.0, -1.0, -1.0, 0.0]
 
 # =======
 # discount
@@ -18,5 +18,5 @@ run_names  = ["SB", "ALD simple", "ALD parallel", "ALD parallel2"]
 ramp_modes = [:None, :simple, :parallel, :parallel2]
 iters      = [100, 200, 200, 200]
 
-export noise, discount, run_names, ramp_modes, iters
+export noise, shift, discount, run_names, ramp_modes, iters
 end
