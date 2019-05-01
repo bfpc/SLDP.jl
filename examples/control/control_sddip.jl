bd845df890709b2c470674200257d16b91443636
Bernardo Freitas Paulo da Costa <bernardo.da-costa@m4x.org>
2019-05-01 16:59:29 -0400

    Penalized version for SDDiP
    This way we don't need to round the incoming noise -
    but I'm still not sure about the meaning of the problem if Lip -> infty.

--- examples/control/control_sddip.jl
+++ examples/control/control_sddip.jl
@@ -7,6 +7,14 @@ import Gurobi: GurobiSolver
 
 function controlmodel(;nstages=8, discount=0.9, noise=noise, eps=0.1, pattern=(0,1,1), lagtol=1e-4)
 
+# ==================
+# Lipschitz constant
+function Lip(t)
+    return (1.0 - discount^(nstages+1-t))/(1 - discount)*discount^(t-1)
+end
+
+# ======
+# Solver
 env = Gurobi.Env()
 
 # ==========
@@ -25,19 +33,24 @@ m = SDDPModel(
     # ========
     # Dynamics
     @variable(sp, 0 <= move <= 1, Int)  # Control: left or right
+    @variable(sp, s) # slack
     # Correct to not have first stage noise
     if stage == 1
-      @constraint(sp, x == x0 + 2*move - 1)
+      @constraint(sp, x == x0 + 2*move - 1 + s)
     else
-      @rhsnoise(sp, drift=noise, x == x0 + drift + 2*move - 1)
+      @rhsnoise(sp, drift=noise, x == x0 + drift + 2*move - 1 + s)
     end
 
-    # ==========================================
-    # Cost: absolute difference from equilibrium
+    # ====================================================================
+    # Cost: absolute difference from equilibrium, plus integrality penalty
     @variable(sp, xpos >= 0)
     @variable(sp, xneg >= 0)
     @constraint(sp, x - 10 == xpos - xneg)
-    @stageobjective(sp, discount^(stage-1)*(xpos + xneg))
+
+    @variable(sp, norm_s)
+    @constraint(sp, norm_s >= s)
+    @constraint(sp, norm_s >= -s)
+    @stageobjective(sp, discount^(stage-1)*(xpos + xneg) + (1+Lip(stage))*norm_s)
 
     # =================================
     # The solver hook for backwards SDDiP
